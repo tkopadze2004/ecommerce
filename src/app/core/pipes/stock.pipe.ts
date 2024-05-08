d345fcd626cb85a6f3dfcd7a38e8e428e8ef77ee
Tamta <tamtakopadz16@gmail.com>
2024-05-08 18:39:11 +0400

    Refactor stock  pipe based on image instock status

--- src/app/core/pipes/stock.pipe.ts
+++ src/app/core/pipes/stock.pipe.ts
@@ -1,13 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'stock',
+  name: 'instock',
   standalone: true
 })
 export class StockPipe implements PipeTransform {
 
   transform(value?: unknown): 'In Stock' | 'Out Of Stock' {
-    return  value? 'In Stock'  : 'Out Of Stock';
-  }
-
+    return value ? 'In Stock' : 'Out Of Stock';
+   }
 }

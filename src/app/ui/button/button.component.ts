11eea0255b296bef0b3a257ba1acf16999040530
Tamta <tamtakopadz16@gmail.com>
2024-05-20 23:49:01 +0400

    add quantity input

--- src/app/ui/button/button.component.ts
+++ src/app/ui/button/button.component.ts
@@ -13,15 +13,18 @@ import { Component, Input, booleanAttribute } from '@angular/core';
     '[class.alte-button--small]': 'size === "small"',
     '[class.alte-button--primary]': 'theme === "primary"',
     '[class.alte-button--outline]': 'theme === "outline"',
+    '[class.alte-button--icon]': 'theme === "icon"',
+    '[class.alte-button--outline-icon]': 'theme === "outline-icon"',
     '[class.alte-button--outlineblack]': 'theme === "outlineblack"',
     '[class.alte-button--outlinesm]': 'theme === "outlinesm"',
+
     '[class.alte-button--block]': 'block'
 
   }
 })
 export class ButtonComponent {
   @Input() size: 'default' | 'small' = 'default'
-  @Input() theme: 'primary' | 'outline'  |'outlineblack'|'outlinesm' = 'primary'
+  @Input() theme: 'primary' | 'outline'  |'outlineblack'|'outlinesm' | 'icon' | 'outline-icon' = 'primary'
   @Input() disabled :boolean=false
   @Input ({transform:booleanAttribute}) block : boolean=false
 }

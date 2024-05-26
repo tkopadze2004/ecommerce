af25ed91773a19c8aa8b18b0649d392531417340
Tamta <tamtakopadz16@gmail.com>
2024-05-26 18:17:30 +0400

    update best seller page

--- src/app/components/best-seller/best-seller.component.ts
+++ src/app/components/best-seller/best-seller.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { BEST_SELLER } from '../../data/bestseller-products';
+import { Component, Input, inject } from '@angular/core';
+import { ProductItemComponent } from "../product-item/product-item.component";
+import { Observable } from 'rxjs';
+import { Products } from '../../core/interfaces.ts/products';
+import { ProductsFacade } from '../../facades/products.facade';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
-  selector: 'app-best-seller',
-  standalone: true,
-  imports: [],
-  templateUrl: './best-seller.component.html',
-  styleUrl: './best-seller.component.scss'
+    selector: 'app-best-seller',
+    standalone: true,
+    templateUrl: './best-seller.component.html',
+    styleUrl: './best-seller.component.scss',
+    imports: [ProductItemComponent,AsyncPipe]
 })
 export class BestSellerComponent {
-  bestseller=BEST_SELLER
+@Input({required:true}) title:string=''
+@Input() shop :string=''
+
+  ProductsFacade=inject(ProductsFacade)
+  latestProducts$: Observable<Products[]> =  this.ProductsFacade.bestSelling()
 
 }

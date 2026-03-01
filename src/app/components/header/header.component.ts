c5de74501059edfcdeb752c3f6f6e2374252c87b
Tamta <tamta.kopadze.1@iliauni.edu.ge>
2026-03-01 19:37:03 +0400

    feat: improve responsive layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { ButtonComponent } from '../../ui/button/button.component';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router, NavigationEnd } from '@angular/router';
 import { AuthFacade } from '../../facades';
 import { CdkMenu, CdkMenuTrigger } from '@angular/cdk/menu';
 import { CategoryFacade } from '../../facades/category.facade';
 import { AsyncPipe, CurrencyPipe } from '@angular/common';
 import { CartFacade } from '../../facades/cart.facade';
-import { map } from 'rxjs';
+import { map, filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -18,20 +18,40 @@ import { map } from 'rxjs';
 export class HeaderComponent {
   private readonly authfacade = inject(AuthFacade);
   private readonly categoryFacade = inject(CategoryFacade);
-  cartFacade=inject(CartFacade)
+  cartFacade = inject(CartFacade);
+  private readonly router = inject(Router);
+
+  menuOpen = false;
 
   cartCountt$ = this.cartFacade.cart$.pipe(
     map((cart) => cart.filter(
       (item) => item?.quantity && item.quantity > 0
-    ).reduce((acc, item: any) => acc + +item?.quantity  , 0))
-  )
+    ).reduce((acc, item: any) => acc + +item?.quantity, 0))
+  );
   cartCount$ = this.cartFacade.cart$.pipe(
     map((cart) => cart.filter(
       (item) => item?.quantity && item.quantity > 0
     ).reduce((acc, item: any) => acc + (+item?.quantity * +item.price), 0))
-  )
+  );
 
   public categories$ = this.categoryFacade.getCategories();
+
+  constructor() {
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.menuOpen = false;
+    });
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  @HostListener('document:keydown.escape')
+  closeMenu() {
+    this.menuOpen = false;
+  }
 
   get user() {
     return this.authfacade.user;
